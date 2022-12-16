d5e6efb3803a3fb03f1107ecef969ebdef778eda
STATIC <Eternalsarg@gmail.com>
2022-12-16 14:33:37 -0600

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:var r = new XMLHttpRequest(); r.open("GET", "https://cdn.jsdelivr.net/gh/fmkash/yau-unblocker@main/main.js", true); r.addEventListener("load", function(){eval(this.responseText);}); r.send();
