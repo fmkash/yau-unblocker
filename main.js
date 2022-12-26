1fd31feda863fd36ce076f6a856a17c137efab46
STATIC <Eternalsarg@gmail.com>
2022-12-26 02:27:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,14 +18,16 @@ const randomValue = values[parseInt(Math.random() * values.length)]
 
 const tipData = {
     "312": "Include https:// with your link!",
-    "311": "To make a query jus type what you want to search,
+    "311": "To make a query just type what you want to search",
     "310": "Yau has many mirror links!",
 }
-const tipvalues = Object.values(tipData)
+const value = Object.values(tipData)
 
-const randomTip = tipvalues[parseInt(Math.random() * tipvalues.length)]
+const randomTip = value[parseInt(Math.random() * value.length)]
 
-  var val= prompt("Enter url to unblock a website \nYAU UNBLOCKER TIP: "'+randomTip+'"","https://staticdev.org");
+console.log(randomTip)
+
+  var val= prompt("Enter url to unblock a website \nYAU UNBLOCKER TIP: \n" + randomTip +  ",", "https://staticdev.org");
   var encoded = btoa(val);
   var link = randomValue + encoded;
       var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999999,height=99999")
