05ccdd4d0ba2289cb968bb6e93090ac0c29a500e
STATIC <Eternalsarg@gmail.com>
2022-12-25 19:58:43 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:var r = new XMLHttpRequest(); r.open("GET", "https://cdn.jsdelivr.net/gh/fmkash/yau-unblocker@main/main.js", true); r.addEventListener("load", function(){eval(this.responseText);}); r.send();
+javascript:var r = new XMLHttpRequest(); r.open("GET", "https://raw.githubusercontent.com/fmkash/yau-unblocker/main/main.js", true); r.addEventListener("load", function(){eval(this.responseText);}); r.send();
