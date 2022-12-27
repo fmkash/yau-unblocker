51a62753c3b80a335a2f77ddcb57c97d29c35935
STATIC <Eternalsarg@gmail.com>
2022-12-26 18:54:52 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var pwd = prompt("Enter the password \nIF YOU DONT KNOW THE PASSWORD, MAKE AN ACCOUNT AT \nhttps://news.staticdev.org AND CREATE A NEW POST ASKING FOR THE PASSWORD\n ", "");
+var pwd = prompt("Enter the password \nIF YOU DONT KNOW THE PASSWORD, MAKE AN ACCOUNT AT \nhttps://news.staticdev.org AND CREATE A NEW POST ASKING FOR THE PASSWORD!\n ", "");
 var epwd = btoa(pwd);
 if (epwd !== "Njc2MA==") {
     alert("Wrong password!");
