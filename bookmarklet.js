e687198c2dd3e4148a8486ecc487f44445e0548c
STATIC <Eternalsarg@gmail.com>
2023-01-10 11:50:45 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -29,17 +29,10 @@ javascript: (function () {
 
     console.log(randomTip)
 
-    var pwd = prompt("Enter the password \nIF YOU DONT KNOW THE PASSWORD, MAKE AN ACCOUNT AT \nhttps://news.staticdev.org AND CREATE A NEW POST ASKING FOR THE PASSWORD!\n ", "");
-    var epwd = btoa(pwd);
-    if (epwd !== "Njc2MA==") {
-        alert("Wrong password!");
-        return false;
-    } else {
         var val = prompt("Enter url to unblock a website \nYAU UNBLOCKER TIP: \n" + randomTip, "https://staticdev.org");
         var encoded = btoa(val);
         var link = randomValue + encoded;
         var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999999,height=99999")
         w.document.write('<script>document.title = "about:blank";</script>' +
             '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + link + '"></iframe>');
-    }
 })();
