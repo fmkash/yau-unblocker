995e8c8b75e4a73c1dc81233ce3e0dfba81a60d7
STATIC <Eternalsarg@gmail.com>
2023-01-20 06:09:34 -0600

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const randomValue = values[parseInt(Math.random() * values.length)]
 var val = prompt("YAU VERSION: V0.0.2 \nEnter url to unblock a website \nMADE BY static#1865", "https://staticdev.org");
 var encoded = btoa(val);
 console.log(encoded)
-if (encoded == 'bnVsbA==', '') {
+if (encoded == 'bnVsbA==' || '') {
     var u = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999999,height=99999")
     u.document.write('<script>document.title = "about:blank";</script>' +
         '<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="https://staticdev.org/yau/yau-error.html"></iframe>');
